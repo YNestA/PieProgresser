6998fefda10eff72aa02ec3a0dbefa1032ca63c7
YNestA <U201417256@hust.edu.cn>
2016-11-06 12:12:12 +0800

    before lunch

---
+++ PieProgresser/js/PieProgress.js
@@ -0,0 +1,9 @@
+function pieRoll($pie,angle) {
+    var $progress=$pie.find("span.progress");
+    if($progress.length){
+        console.log(1);
+        console.log($progress.css("transform","rotate(60deg)"));
+    }
+}
+$(document).ready(function () {
+});
--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 /**
  * Created by yang on 16-11-5.
  */
+$(document).ready(function () {
+    pieRoll($("#my_pie_progresser"),60);
+});
