8c11ff4c9f2f7ec89c5c022aa430abbef80a0040
YNestA <U201417256@hust.edu.cn>
2016-11-07 16:35:04 +0800

    11.7

--- PieProgresser/js/PieProgress.js
+++ PieProgresser/js/PieProgress.js
@@ -1,8 +1,72 @@
-function pieRoll($pie,angle) {
-    var $progress=$pie.find("span.progress");
-    if($progress.length){
-        console.log(1);
-        console.log($progress.css("transform","rotate(60deg)"));
+var ROLLTIME=1000,
+    progressColor="rgb(130,57,53)";
+function pieRoll($pie,angle,time) {
+    var $leftProgress=$pie.find("span.left_progress"),
+        $rightProgress=$pie.find("span.right_progress");
+    time=time?time:ROLLTIME;
+    angle=angle==360?360:Math.abs(angle)%360;
+    if($leftProgress.length&&$rightProgress.length){
+        var lastAngle=parseInt($pie.attr("data-angle"));
+        var startPercents=parseInt(lastAngle/360*100),
+            endPercents=parseInt(angle/360*100),
+            currentPercents=startPercents;
+        var intervalID=setInterval(function () {
+            if (startPercents<endPercents){
+                currentPercents+=1;
+            }else{
+                currentPercents-=1;
+            }
+            $pie.find("div.progress_num").text(currentPercents+"%");
+            if(currentPercents===endPercents){
+                clearInterval(intervalID);
+            };
+        },Math.abs(time/(endPercents-startPercents)));
+        if(angle<180){
+            if(lastAngle<180){
+                $rightProgress.css({
+                    "transform":"rotate("+angle+"deg)",
+                    "transition":"transform "+time+"ms linear",
+                });
+            }else{
+                var firstTime=(lastAngle-180)/(lastAngle-angle)*time,
+                    secondTime=time-firstTime;
+                $leftProgress.css({
+                    "transform":"rotate(0deg)",
+                    "transition":"transform "+firstTime+"ms linear",
+                })
+                setTimeout(function(){
+                    $leftProgress.css("background-color","transparent");
+                    $rightProgress.css({
+                        "background-color":"inherit",
+                        "transform":"rotate("+angle+"deg)",
+                        "transition":"transform "+secondTime+"ms linear"
+                    });
+                },firstTime);
+            }
+        }else{
+            if(lastAngle>180){
+                $leftProgress.css({
+                    "transform":"rotate("+(angle-180)+"deg)",
+                    "transition":"transform "+time+"ms linear",
+                });
+            }else{
+                var firstTime=(180-lastAngle)/(angle-lastAngle)*time,
+                    secondTime=time-firstTime;
+                $rightProgress.css({
+                    "transform":"rotate(180deg)",
+                    "transition":"transform "+firstTime+"ms linear",
+                });
+                setTimeout(function(){
+                    $rightProgress.css("background-color","transparent");
+                    $leftProgress.css({
+                        "background-color":progressColor,
+                        "transform":"rotate("+(angle-180)+"deg)",
+                        "transition":"transform "+secondTime+"ms linear",
+                    });
+                },firstTime);
+            }
+        }
+        $pie.attr("data-angle",angle);
     }
 }
 $(document).ready(function () {
--- index.js
+++ index.js
@@ -2,5 +2,28 @@
  * Created by yang on 16-11-5.
  */
 $(document).ready(function () {
-    pieRoll($("#my_pie_progresser"),60);
+    var $myPieProgresser=$("#my_pie_progresser");
+    //pieRoll($myPieProgresser,50,1000);
+    	pieRoll($myPieProgresser,90,2000);
+    	setTimeout(function(){
+    		pieRoll($myPieProgresser,210,2000);
+    	},2000);
+    	setTimeout(function(){
+    		pieRoll($myPieProgresser,300,2000);
+    	},4000);
+    	setTimeout(function(){
+    		pieRoll($myPieProgresser,360,2000);
+    	},6000);
+    	setTimeout(function(){
+    		pieRoll($myPieProgresser,300,2000);
+    	},8000);
+    	setTimeout(function(){
+    		pieRoll($myPieProgresser,210,2000);
+    	},10000);
+    	setTimeout(function(){
+    		pieRoll($myPieProgresser,90,2000);
+    	},12000);
+    	setTimeout(function(){
+    		pieRoll($myPieProgresser,10,2000);
+    	},14000);
 });
